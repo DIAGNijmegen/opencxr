d2d924306fe84535bd4ebf48bc6b4f2fc68c8baa
Keelin Murphy <keelinm@gmail.com>
2020-07-16 12:13:58 +0100

    added test for image sorter

---
+++ tests/test_imagesorter.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 15:40:35 2020
+
+@author: keelin
+"""
+
+import opencxr
+from pathlib import Path
+
+# Load the algorithm
+# possible algorithms are listed in opencxr/algorithms/__init__.py
+img_sorter_algorithm = opencxr.load(opencxr.algorithms.image_sorter)
+
+# test with input and output folders
+f_in = Path(__file__).parent / "resources" / "images"
+f_out = Path(__file__).parent / "resources" / "images"
+img_sorter_algorithm.run(f_in, f_out)
+
+# TODO: test with input and output files
