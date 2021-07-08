60068804acb683f1c2c874017e18b1562e80741c
Keelin Murphy <keelinm@gmail.com>
2021-07-08 14:58:31 +0100

    fixed lung segmentation testing

--- tests/test_lungsegmentation.py
+++ tests/test_lungsegmentation.py
@@ -6,17 +6,18 @@
 
 import opencxr
 from pathlib import Path
-from opencxr.utils.file_io import read_file
+from opencxr.utils.file_io import read_file, write_file
 
 # Load the algorithm
 # possible algorithms are listed in opencxr/algorithms/__init__.py
 lungseg_algorithm = opencxr.load(opencxr.algorithms.lung_seg)
 # test with numpy array as input
-f_in = Path(__file__).parent / "resources" / "images" / "test_img1.png" 
+f_in = Path(__file__).parent / "resources" / "images" / "g0536.mha"
+f_in = str(f_in.resolve())
 img,spacing,pydata = read_file(f_in)
 seg_map = lungseg_algorithm.run(img)
-
-
-# TODO: test with input and output files
-#lungseg_algorithm.run_write('opencxr/tests/resources/images/', 'result_folder/')
+f_out = Path(__file__).parent / "resources" / "tmp_test_outputs" / "g0536.mha"
+f_out = str(f_out.resolve())
+print(seg_map.dtype)
+write_file(f_out, seg_map, spacing)
 
